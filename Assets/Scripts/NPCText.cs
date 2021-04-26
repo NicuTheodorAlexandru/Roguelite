cf1495563f2047749a4c4b3182de56f7f580010e
Nicu Theodor Alexandru <nicu.andru@yahoo.com>
2021-04-27 00:19:09 +0300

    Implemented basic NPC

---
+++ Assets/Scripts/NPCText.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    class NPCText
+    {
+        TextOptions textOptions;
+
+        public class TextTree
+        {
+            
+        }
+
+        public class TextOptions
+        {
+            private Dictionary<string, string[]> textOptions;
+
+            public TextOptions(Dictionary<string, string[]> text)
+            {
+                textOptions = text;
+            }
+        }
+    }
+}
