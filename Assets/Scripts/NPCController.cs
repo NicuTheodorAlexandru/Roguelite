ad5f881813f8e9a6741057e3ced7ded9daf84fbe
Nicu Theodor Alexandru <nicu.andru@yahoo.com>
2021-06-09 00:18:47 +0300

    Added text speed

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Assets.Scripts;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +7,7 @@ using UnityEngine.UI;
 public class NPCController : MonoBehaviour
 {
     public Text text;
+    private NPCText npcText;
     public GameObject hintImage;
     public string[] textLines;
     private int textProgress;
@@ -15,33 +17,40 @@ public class NPCController : MonoBehaviour
     {
         isFocused = false;
         textProgress = 0;
-        text.text = textLines[0];
+        //text.text = textLines[0];
         GetComponentInChildren<Canvas>().worldCamera = Camera.main;
+        npcText = new NPCText(text, textLines, 15);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (PauseMenu.paused)
+            return;
         if (!isFocused)
             return;
         if(Input.GetKeyDown(KeyCode.E) && hintImage.activeSelf)
         {
             hintImage.SetActive(false);
             text.gameObject.SetActive(true);
+            npcText.LoadNext();
         }
         else if(Input.GetKeyDown(KeyCode.E) && text.gameObject.activeSelf)
         {
             if(textProgress < textLines.Length - 1)
             {
-                textProgress++;
-                text.text = textLines[textProgress];
+                //textProgress++;
+                //text.text = textLines[textProgress];
+                npcText.LoadNext();
             }
         }
+        npcText.Update();
     }
 
     private void ResetText()
     {
-        text.text = textLines[0];
+        //text.text = textLines[0];
+        npcText.Reset();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
--- Assets/Scripts/NPCText.cs
+++ Assets/Scripts/NPCText.cs
@@ -3,26 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
     class NPCText
     {
-        TextOptions textOptions;
+        public Text text;
+        string[] textLines;
+        int textIndex;
+        int charIndex;
+        string currentText;
+        int textSpeed;
+        int frames;
+
+        public NPCText(Text text, string[] textLines, int textSpeed)
+        {
+            textIndex = -1;
+            charIndex = 0;
+            currentText = "";
+            frames = 0;
+            this.text = text;
+            this.textLines = textLines;
+            this.textSpeed += textSpeed;
+        }
+
+        public void Reset()
+        {
+            textIndex = -1;
+            charIndex = 0;
+            currentText = "";
+            frames = 0;
+        }
+
+        public void LoadNext()
+        {
+            if (textIndex >= textLines.Length - 1)
+                return;
+            textIndex++;
+            charIndex = 0;
+            currentText = "";
+        }
+
+        public void Update()
+        {
+            LoadText();
+            frames++;
+        }
+
+        private void LoadText()
+        {
+            if(frames < textSpeed)
+            {
+                return;
+            }
+            if (textIndex < 0)
+            {
+                return;
+            }
+            if (charIndex >= textLines[textIndex].Length)
+            {
+                return;
+            }
+            currentText += textLines[textIndex][charIndex];
+            text.text = currentText;
+            charIndex++;
+            frames = 0;
+        }
 
         public class TextTree
         {
             
-        }
-
-        public class TextOptions
-        {
-            private Dictionary<string, string[]> textOptions;
-
-            public TextOptions(Dictionary<string, string[]> text)
-            {
-                textOptions = text;
-            }
         }
     }
 }
